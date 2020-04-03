c6abde7357264654036a5b557d188ba7662323f2
miwiley <53870716+miwiley@users.noreply.github.com>
2020-04-03 12:17:26 -0700

    Delete open_covid19_tabs.py

--- python/open_covid19_tabs.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-url_google = "https://www.google.com/search?q=covid+19"
-url_yahoo = "https://search.yahoo.com/search?p=covid+19"
-url_covid19data_us = "http://graphs.covid19data.us"
-
-
-webbrowser.open_new_tab(url_google)
-webbrowser.open_new_tab(url_yahoo)
-webbrowser.open_new_tab(url_covid19data_us)
